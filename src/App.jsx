3ef5f36f863a63b28648fdb0e439a3b0780a6982
Kevin Ng <kevin.the.engi@gmail.com>
2021-04-25 15:59:59 -0700

    Convert to hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,38 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 import CardList from './components/CardList/CardList.jsx';
 import Search from './components/Search/Search.jsx';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [search, setSearch] = useState('');
 
-    this.state = {
-      monsters: [],
-      search: ''
-    }
-
-    this.searchMonsters = this.searchMonsters.bind(this);
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => this.setState({
-        monsters: data
-      }))
+    .then(response => response.json())
+    .then(data => setMonsters(data))
+  })
+
+  const searchMonsters = (e) => {
+    const { value } = e.target;
+
+    setSearch(value);
   }
 
-  searchMonsters(e) {
-    const { name, value } = e.target;
-    
-    this.setState({
-      [name]: value
-    })
-  }
+  const filteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(search.toLowerCase())
+  )
 
-  render() {
-    const { monsters, search } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(search.toLowerCase())
-    )
-
-    return (
-      <div className="App">
-        <Search
-          placeholder="Search monsters..."
-          filter={this.searchMonsters}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Search
+        placeholder="Search monsters..."
+        filter={searchMonsters}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,48 +1,10 @@
 import React from 'react';
 import './Search.css';
 
-// class Search extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       search: ''
-//     }
-
-//     this.searchHandle = this.searchHandle.bind(this);
-//   }
-
-//   searchHandle(e) {
-//     e.preventDefault();
-
-//     const { name, value } = e.target;
-
-//     this.setState({
-//       [name]: value
-//     }, () => {
-//       this.props.filter(this.state.search);
-//     })
-//   }
-
-//   render () {
-//     const { search } = this.state;
-
-//     return (
-//       <div className="search">
-//         <input
-//           type="text"
-//           name="search"
-//           value={search}
-//           onChange={this.searchHandle}
-//         />
-//       </div>
-//     )
-//   }
-// }
-
 const Search = ({ placeholder, filter }) => {
   return (
     <input
+      className="search"
       type="text"
       name="search"
       placeholder={placeholder}
